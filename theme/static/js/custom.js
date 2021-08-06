053e2e627076a247114a6bc3b6e4941d88f5b8d1
hankliao87 <hankliao87@gmail.com>
2021-08-06 22:58:13 +0800

    Update css file

--- theme/static/js/custom.js
+++ theme/static/js/custom.js
@@ -1,5 +1,4 @@
 $(function () {
-    console.log('run');
     $('table').each(function () {
         $(this).addClass('table table-sm table-hover');
         $(this).wrap('<div class="table-responsive"></div>');
